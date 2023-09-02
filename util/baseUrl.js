cd4e6cfb15b54539a4d113197fc2a4596fc70bc1
Prateek Singh <ps0431299#gmail.com>
2023-09-02 15:47:17 +0530

    url

--- util/baseUrl.js
+++ util/baseUrl.js
@@ -1,2 +1,2 @@
-const baseUrl = process.env.NODE_ENV === 'production'? "myEcomm2031299.vercel.app": "http://localhost:3000"
+const baseUrl = process.env.NODE_ENV === 'production'? "https://ecommerce-ls5m71nrf-ps0431299-gmailcom.vercel.app": "http://localhost:3000"
 export default baseUrl
