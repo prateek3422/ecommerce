d62b8d9e9687e9357e5c63780023fec5336109f2
Prateek Singh <ps0431299#gmail.com>
2023-09-03 21:12:44 +0530

    all fix

--- pages/order.jsx
+++ pages/order.jsx
@@ -12,14 +12,12 @@ const order = ({ orders }) => {
   return (
     <>
       <section className="text-gray-600 body-font overflow-hidden">
-
         <div className="flex justify-center items-center flex-col mb-16  ">
           <h1>Your Order</h1>
         </div>
 
-        {
-          orders.length === 0 ? (
-            <div className="cart-empty">
+        {orders.length === 0 ? (
+          <div className="cart-empty">
             <div className="carts">
               <h1>Your order is Emapty</h1>
             </div>
@@ -30,66 +28,62 @@ const order = ({ orders }) => {
               </Link>
             </div>
           </div>
-          ): (
-        <div className="container px-5 py-16 mx-auto">
-          {orders?.map((item) => {
-            return (
-              <div key={item._id}>
-              {item?.products?.map((product) => {
-                      return (
-                <div className="lg:w-4/5 mx-auto flex flex-wrap">
-                  <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
-                      // console.log(product);
-                        <div key={product._id}>
-                          <h2 className="text-sm title-font text-gray-500 tracking-widest">
-                            {product.product.name}
-                          </h2>
-                          <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">
-                            Animated Night Hill Illustrations
-                          </h1>
-                          <div className="flex mb-4">
-                            <a className="flex-grow text-indigo-500 border-b-2 border-indigo-500 py-2 text-lg px-1">
-                              Description
-                            </a>
-                          </div>
-                          <p className="leading-relaxed mb-4">
-                            {product.product.description}
-                          </p>
-                          <div className="flex border-t border-b mb-6 border-gray-200 py-2 justify-between">
-                            <span className="text-gray-500">Quantity</span>
+        ) : (
+          <div className="container px-5 py-16 mx-auto">
+            {orders?.map((item) => {
+              return (
+                <div key={item._id}>
+                  {item?.products?.map((product) => {
+                    return (
+                      <div key={product.Id} className="lg:w-4/5 mx-auto flex flex-wrap">
+                        <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+                          // console.log(product);
+                          <div>
+                            <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                              {product.product.name}
+                            </h2>
+                            <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">
+                              Animated Night Hill Illustrations
+                            </h1>
+                            <div className="flex mb-4">
+                              <a className="flex-grow text-indigo-500 border-b-2 border-indigo-500 py-2 text-lg px-1">
+                                Description
+                              </a>
+                            </div>
+                            <p className="leading-relaxed mb-4">
+                              {product.product.description}
+                            </p>
+                            <div className="flex border-t border-b mb-6 border-gray-200 py-2 justify-between">
+                              <span className="text-gray-500">Quantity</span>
 
-                            <span className=" ml-auto text-gray-900">
-                              {product.quantity}
+                              <span className=" ml-auto text-gray-900">
+                                {product.quantity}
+                              </span>
+                            </div>
+                          </div>
+                          <div className="flex">
+                            <span className="title-font font-medium text-2xl text-gray-900">
+                              {item.total}
                             </span>
+                            <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                              <Link href="/">Home</Link>
+                            </button>
                           </div>
                         </div>
-
-
-
-                    <div className="flex">
-                      <span className="title-font font-medium text-2xl text-gray-900">
-                        {item.total}
-                      </span>
-                      <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                        <Link href="/">Home</Link>
-                      </button>
-                    </div>
-                  </div>
-                  <img
-                    alt="ecommerce"
-                    className="lg:w-1/2 w-full object-cover object-center rounded"
-                    src={product.product.imageUrl}
-                    />
-                </div>
+                        <img
+                          alt="ecommerce"
+                          className="lg:w-1/2 w-full object-cover object-center rounded"
+                          src={product.product.imageUrl}
+                        />
+                      </div>
                     );
-              })}
-                <hr className=" mb-6 border-gray-200 py-2 " />
-              </div>
-            );
-          })}
-
-        </div>
-          )}
+                  })}
+                  <hr className=" mb-6 border-gray-200 py-2 " />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </section>
     </>
   );
